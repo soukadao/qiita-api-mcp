3a64ae3c211aef0a602e9a18dd013c7470886e3f
soukadao <soukadao@example.com>
2025-09-16 13:49:06 +0900

    version: 1.0.4

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -2,4 +2,4 @@ import { env } from "node:process";
 
 export const BASE_URL = "https://qiita.com/api/v2";
 export const QIITA_API_ACCESS_TOKEN = env.QIITA_API_ACCESS_TOKEN;
-export const VERSION = "1.0.2";
+export const VERSION = "1.0.4";
